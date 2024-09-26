968cb88997d511d90470955c2211e8106ba003e6
Kamil Kamiński <kamilkaminski39@gmail.com>
2024-09-26 18:56:45 +0200

    chore: Backend and frontend logs

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Starting server"
-daphne -b 0.0.0.0 -p 8000 backend.asgi:application
+daphne -b 0.0.0.0 -p 8000 backend.asgi:application --access-log logs/daphne/access.log
 
 exec "$@"
